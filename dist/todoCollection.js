153882929d338be9f1890d2f7474a1811ce5c9c0
kasia.kordys <kasia.kordys@gmail.com>
2021-06-11 22:31:41 +0100

    use Map to store todoitems

--- dist/todoCollection.js
+++ dist/todoCollection.js
@@ -6,16 +6,23 @@ class TodoCollection {
         this.userName = userName;
         this.todoItems = todoItems;
         this.nextId = 1;
+        // var Map: MapConstructor, new () => Map<any, any> 
+        this.itemMap = new Map();
+        // (method) Map<number, TodoItem>.set(key: number, value: TodoItem): Map<number, TodoItem>
+        todoItems.forEach(item => this.itemMap.set(item.id, item));
     }
     addTodo(task) {
         while (this.getToDoById(this.nextId)) {
             return this.nextId++;
         }
-        this.todoItems.push(new todoItem_1.TodoItem(this.nextId, task));
+        // this.todoItems.push(new TodoItem (this.nextId, task));
+        this.itemMap.set(this.nextId, new todoItem_1.TodoItem(this.nextId, task));
         return this.nextId;
     }
     getToDoById(id) {
-        return this.todoItems.find(item => item.id === id);
+        // (method) Map<number, TodoItem>.get(key: number): TodoItem
+        return this.itemMap.get(id);
+        //return this.todoItems.find(item => item.id === id);
     }
     markComplete(id, complete) {
         const todoItem = this.getToDoById(id);
--- src/todoCollection.ts
+++ src/todoCollection.ts
@@ -2,17 +2,27 @@ import  { TodoItem } from "./todoItem";
 
 export class TodoCollection {
     private nextId: number = 1;
-    constructor(public userName: string, public todoItems: TodoItem[] = []) {}
+
+    // var Map: MapConstructor, new () => Map<any, any> 
+    private itemMap = new Map<number, TodoItem>(); 
+
+    constructor(public userName: string, public todoItems: TodoItem[] = []) {
+        // (method) Map<number, TodoItem>.set(key: number, value: TodoItem): Map<number, TodoItem>
+        todoItems.forEach(item => this.itemMap.set(item.id, item));
+    }
 
     addTodo(task: string) : number {
         while (this.getToDoById(this.nextId)) {
             return this.nextId++;
         }
-        this.todoItems.push(new TodoItem (this.nextId, task));
+        // this.todoItems.push(new TodoItem (this.nextId, task));
+        this.itemMap.set(this.nextId, new TodoItem(this.nextId, task));
         return this.nextId;
     }
     getToDoById(id: number) : TodoItem {
-        return this.todoItems.find(item => item.id === id);
+        // (method) Map<number, TodoItem>.get(key: number): TodoItem
+        return this.itemMap.get(id);
+        //return this.todoItems.find(item => item.id === id);
     }
     markComplete(id: number, complete: boolean) {
         const todoItem = this.getToDoById(id);
