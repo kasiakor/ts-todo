5186897c8a9ed8574874eac981543664d6c9fef8
kasia.kordys <kasia.kordys@gmail.com>
2021-06-15 12:15:39 +0100

    add method to display todo list

--- src/index.ts
+++ src/index.ts
@@ -9,21 +9,9 @@ let todos: TodoItem[]  = [
 let collection: TodoCollection = new TodoCollection("Gia", todos);
 
 console.clear();
-console.log(`${collection.userName}'s Todo List` + ` (${collection.getItemsCount().incomplete} items to do)`);
 
-// let newTaskId: number = collection.addTodo("go to bank");
-// returns number "1"
-// console.log(newTaskId);
-
-// let newTask: TodoItem = collection.getToDoById(newTaskId);
-// returns first item with passed id
-// TodoItem { id: 1, task: 'buy flour', complete: false }
-// console.log(newTask);
-
-// newTask.printDetails();
-
-// collection.addTodo(newTaskId);
-
-// getTodoItems = TodoItem[]
 collection.removeComplete();
-collection.getTodoItems(true).forEach(item => item.printDetails());
+function displayTodoList(): void {
+    console.log(`${collection.userName}'s Todo List` + ` (${collection.getItemsCount().incomplete} items to do)`);
+    collection.getTodoItems(true).forEach(item => item.printDetails());
+}
